3183379af18f3ef1bcbb697cdf5d47809f31aa12
Shamistan Huseynov <shamistan.huseynov@abb-bank.az>
2022-05-25 22:17:25 +0400

    optimizations in UserService

--- src/main/java/com/da/p4/project/service/UserService.java
+++ src/main/java/com/da/p4/project/service/UserService.java
@@ -1,7 +1,6 @@
 package com.da.p4.project.service;
 
 import com.da.p4.project.dto.UserDto;
-import com.da.p4.project.mapper.UserMapper;
 import com.da.p4.project.model.User;
 import com.da.p4.project.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
@@ -22,7 +21,7 @@ public class UserService {
 
     private final UserRepo userRepo;
 
-   // private final UserMapper userMapper;
+    // private final UserMapper userMapper;
 
     public List<User> getUsers() {
         return userRepo.findAll();
@@ -31,34 +30,35 @@ public class UserService {
     public Optional<User> getUser(Long id) {
         return userRepo.findById(id);
     }
-    public void updateUser(UserDto userDto, Long userId) {
-         userRepo.findById(userId).ifPresentOrElse(u->{
-          //   User user = userMapper.toUserModel(userDto);
 
-             User user = new User();
-             user.setId(userDto.getId());
-             user.setUsername(userDto.getUsername());
-             user.setPassword(userDto.getPassword());
-             user.setRole(userDto.getRole());
-             userRepo.save(user);
-         }, ()-> {throw new RuntimeException("User cannot be found");});
+    public void updateUser(UserDto userDto, Long userId) {
+        userRepo.findById(userId).ifPresentOrElse(u -> {
+            //   User user = userMapper.toUserModel(userDto);
+            u.setUsername(userDto.getUsername() != null ? userDto.getUsername() : u.getUsername());
+            u.setPassword(userDto.getPassword()!= null ? userDto.getPassword() : u.getPassword());
+            u.setFullName(userDto.getFullName()!= null ? userDto.getFullName() : u.getFullName());
+            u.setRole(userDto.getRole()!= null ? userDto.getRole() : u.getRole());
+            userRepo.save(u);
+        }, () -> {
+            throw new RuntimeException("User cannot be found");
+        });
     }
-    public String deleteUser(Long id){
+
+    public String deleteUser(Long id) {
         try {
             userRepo.deleteById(id);
             return SUCCESS;
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error("Following error occurred " + e.getMessage());
             return FAILED;
         }
-
     }
 
-    public String deleteAll(){
+    public String deleteAll() {
         try {
             userRepo.deleteAll();
             return SUCCESS;
-        }catch (Exception exception){
+        } catch (Exception exception) {
             log.error("Following error occurred " + exception.getMessage());
             return FAILED;
         }
@@ -66,7 +66,7 @@ public class UserService {
 
     public String addUser(UserDto userDto) {
         try {
-       //     User user = userMapper.toUserModel(userDto);
+            //     User user = userMapper.toUserModel(userDto);
 
             User user = new User();
             user.setId(userDto.getId());
@@ -76,7 +76,7 @@ public class UserService {
             user.setRole(userDto.getRole());
             userRepo.save(user);
             return SUCCESS;
-        } catch (Exception exception){
+        } catch (Exception exception) {
             log.error("Following error occurred " + exception.getMessage());
             return FAILED;
         }
